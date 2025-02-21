b68925c30a821ec5f095225530ed2dd475d4eb5c
mayankdahotre <mayank.dahotre@gmail.com>
2025-02-21 13:21:28 +0000

    Deploying to gh-pages from @ mayankdahotre/mayankdahotre.github.io@d0ce8f1079720d8e7727c79bf7ef1f3cce2d124a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -478,7 +478,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%61%79%61%6E%6B.%64%61%68%6F%74%72%65@%69%69%74%67%6E.%61%63.%69%6E", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/mayankdahotre", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -488,6 +495,20 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
+        id: 'social-instagram',
+        title: 'Instagram',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://instagram.com/mayank_dahotre", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/mayank-dahotre-a4671223b/", "_blank");
+        },
+      },{
         id: 'social-rss',
         title: 'RSS Feed',
         section: 'Socials',
@@ -495,18 +516,25 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://twitter.com/Mayank Dahotre", "_blank");
+        },
+      },{
+        id: 'social-youtube',
+        title: 'YouTube',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://youtube.com/@mayankdahotre1302", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://mayankdahotre.github.io", "_blank");
         },
       },{
       id: 'light-theme',
